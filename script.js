85d9c67d48cfac7f293d7ded08d7b1362e1741c8
julianojnc <100528900+julianojnc@users.noreply.github.com>
2023-10-18 11:56:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+/* Menu */
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
-}
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
+
+/* Esconder NavBar */
+const lastScrollTop = 0;
+bar = document.getElementById("bar");
+window.addEventListener("scroll", function () {
+  const scrollTop =
+    this.window.pageXOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    bar.style.top = "-68px";
+    navbar.style.top = "-295%"
+  } else {
+    bar.style.top = "0";
+    navbar.style.top = "100%";
+  }
+  lastScrollTop = scrollTop;
+});
